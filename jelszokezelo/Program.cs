b70734a32b801649c2598eb45d3ceb43f7ab22c5
Mojzi PC <Mojzi@DESKTOP-CG1M4VP>
2024-01-27 11:20:52 +0100

    javitani kell a szam ellenorzot

--- jelszokezelo/Program.cs
+++ jelszokezelo/Program.cs
@@ -1,28 +1,44 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace jelszokezelo
 {
     internal class Program
     {
+        static string[] chars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", "|", ":", ";", "'", "<", ">", ",", ".", "?", "/" };
+        static int index = 0;
+
         static void Main(string[] args)
         {
             string jelszo = "";
             byte szam = 0;
             byte szamE;
-
-            string chars = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&*/\\?";
+    
             byte length = byte.Parse(Console.ReadLine());
 
             for (int i = 0; i < length; i++)
             {
-                Random r = new Random();
-                int index = r.Next(0, 71);
-
-                //byte.TryParse(chars[index], out szamE);
-                jelszo += chars[index];
+                Generator();
+                if (!byte.TryParse(chars[index], out szamE) && i == length - 1)
+                {
+                    Generator();
+                    Console.WriteLine("NIGA");
+                }
+                else
+                {
+                    szam++;
+                    jelszo += chars[index];
+                    
+                }             
             }
 
             Console.WriteLine(jelszo);
+        }
+
+        static void Generator()
+        {
+            Random r = new Random();
+            index = r.Next(0, chars.Count());
         }
     }
 }
