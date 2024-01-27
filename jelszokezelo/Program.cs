96eda2c9ea945d231e2c478120e303e4a671d661
Mojzi PC <Mojzi@DESKTOP-CG1M4VP>
2024-01-27 10:54:23 +0100

    működő generátor

--- jelszokezelo/Program.cs
+++ jelszokezelo/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace jelszokezelo
+﻿using System;
+
+namespace jelszokezelo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string jelszo = "";
+            byte szam = 0;
+            byte szamE;
+
+            string chars = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&*/\\?";
+            byte length = byte.Parse(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(0, 71);
+
+                //byte.TryParse(chars[index], out szamE);
+                jelszo += chars[index];
+            }
+
+            Console.WriteLine(jelszo);
         }
     }
 }
