b00049592b134f70541e5280fc3817085850fabe
KiralyMarcell <98584918+KiralyMarcell@users.noreply.github.com>
2024-01-28 17:25:38 +0100

    navigálómmenü Menu() és jelszólekérő Query() függvény, valamint struktúra létrehozva az adatok eltárolására ami egy listában valósul meg a Load() függvényben, jelenleg 3 adatot tárolunk el és a jelszó lekérés az hely alapján történik pl.: github. Query() függvényben az else ág ki van kommentelve mert azt kellene megoldani, ha nem talál jelszót az adott helyhez akkor újra be lehessen írni egy hibaüzenet után (Do not exxist...)

--- jelszokezelo/Program.cs
+++ jelszokezelo/Program.cs
@@ -6,6 +6,8 @@ namespace jelszokezelo
 {
     internal class Program
     {
+        static List<Passwords> passwords = new List<Passwords>();
+
         static string[] chars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", ":", ";", "'", "<", ">", ",", ".", "?", "/", "|" };
         static int index = 0; //random választott betűk indexe
 
@@ -16,9 +18,14 @@ namespace jelszokezelo
         static byte passLength = 0;
      
         static string pass = ""; //jelszó változó
-
+        
+       
         static void Main()
         {
+            Load("n.txt");
+            Menu();
+            
+            /*
             Input();
 
             LengthIndexGenerator(); //Erre az indexre tesz majd FIXEN egy számot
@@ -26,6 +33,37 @@ namespace jelszokezelo
             Generator(); //jelszó generálása     
             
             Save(); //jelszó mentése egy txt fájlba
+            */
+
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("Options: \t | 0: Generate new password | 1: Show a password | ESC: Exit");
+
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(false);
+
+            switch (consoleKeyInfo.Key)
+            {
+                case ConsoleKey.D0:
+                    Console.Clear();
+                    Input();
+                    LengthIndexGenerator();  //Erre az indexre tesz majd FIXEN egy számot
+                    Generator();             //jelszó generálása
+                    Save();                  //jelszó mentése egy txt fájlba
+                    Menu();                  //navigálómenü
+                    break;
+                case ConsoleKey.D1:
+                    Console.Clear();
+                    Query();                 //Jelszólekérés
+                    break;
+                case ConsoleKey.Escape:
+                    Console.Clear();
+                    Console.WriteLine("Exiting...");
+                    break;
+                default:
+                    break;
+            }
         }
 
         static void Input() //Jelszóhosszúság ellenőrzése
@@ -99,13 +137,16 @@ namespace jelszokezelo
 
             StreamWriter sw = new StreamWriter("n.txt", true); //true - hozzáír a fájlhoz    
 
-            switch (ans)
+            switch (ans.ToUpper())
             {
                 case "Y":
                     Console.Write("Username/Email: ");
                     string usern = Console.ReadLine();
 
-                    sw.WriteLine($"{usern} {pass}");
+                    Console.WriteLine("Location: ");
+                    string location = Console.ReadLine();
+
+                    sw.WriteLine($"{usern} {pass} {location}");
                     sw.Close();
 
                     Console.Clear();
@@ -114,18 +155,73 @@ namespace jelszokezelo
                 case "N":
                     Console.Write("Do you want to generate new one? Y/N ");
                     string gen = Console.ReadLine();
-                    if (gen == "Y")
+                    if (gen.ToUpper() == "Y")
                     {
                         pass = ""; //törli a változó értékét
                         Console.Clear();
                         sw.Close(); //zárja a fájlt, különben nem tudná újra megnyitni
-                        Main();
+                        Menu();
                     }
-                    else Console.WriteLine("Exiting...");
+                    else Console.WriteLine("Exiting...");                                           
                     break;
                 default:
+                    Save(); 
                     break;
             }
         }
+
+        static void Load(string file)
+        {
+            StreamReader sr = new StreamReader(file);
+
+
+            while (!sr.EndOfStream)
+            {                
+                string row = sr.ReadLine();
+                string[] data = row.Split(" ");
+
+                Passwords pass = new Passwords();
+
+                pass.username = data[0];
+                pass.password = data[1];
+                pass.location = data[2];
+
+                passwords.Add(pass);
+
+            }
+            sr.Close();
+        }
+
+        static void Query()
+        {
+            Console.WriteLine("Saved passwords from: ");
+
+            foreach (Passwords item in passwords)
+            {
+                //Console.Clear();
+                Console.WriteLine(item.location);               
+            }
+            Console.WriteLine();
+            Console.Write("Query password: ");             
+            string input = Console.ReadLine();
+
+            foreach (Passwords item in passwords)
+            {
+                if (item.location == input)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Username: {item.username} - Password: {item.password} - Location: {item.location} \n");                    
+                    Menu();
+                }
+                /*else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Do not exist! Try again :(");
+                    Query();
+                }
+                */
+                
+            }
+        }       
     }
 }
