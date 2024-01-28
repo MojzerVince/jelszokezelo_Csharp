972e45aeed7a5fc1b021ea2d19f03d0d0c9f3b51
Mojzi PC <Mojzi@DESKTOP-CG1M4VP>
2024-01-28 08:13:51 +0100

    Input()

--- jelszokezelo/Program.cs
+++ jelszokezelo/Program.cs
@@ -6,7 +6,7 @@ namespace jelszokezelo
 {
     internal class Program
     {
-        static string[] chars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", "|", ":", ";", "'", "<", ">", ",", ".", "?", "/" };
+        static string[] chars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", ":", ";", "'", "<", ">", ",", ".", "?", "/", "|" };
         static int index = 0; //random választott betűk indexe
 
         //jelszó random indexének eltárolására felvett változó (random szám a <LengthIndexGenerator()> függvényből)
@@ -19,13 +19,40 @@ namespace jelszokezelo
 
         static void Main(string[] args)
         {
-            passLength = byte.Parse(Console.ReadLine()); //jelszó hosszúságának bekérése
+            Input();
 
             LengthIndexGenerator(); //Erre az indexre tesz majd FIXEN egy számot
 
-            Generator(); //jelszó generálása
+            Generator(); //jelszó generálása     
             
             Save(); //jelszó mentése egy txt fájlba
+        }
+
+        static void Input()
+        {
+            bool legit = false;
+
+            while(!legit)
+            try
+            {
+                Console.WriteLine("Passlength  between 8-20");
+                Console.Write("Password Length: ");
+                passLength = byte.Parse(Console.ReadLine()); //jelszó hosszúságának bekérése
+                legit = true;
+
+                while (passLength < 8 || passLength > 20) //ha kisebb mint 8 vagy nagyobb mint 20 bekéri újra
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter a pass length above 7 and below 21!");
+                    Console.Write("Password Length: ");
+                    passLength = byte.Parse(Console.ReadLine());
+                }
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("Plese enter a number!");
+            }
         }
 
         static void Generator()
@@ -62,7 +89,7 @@ namespace jelszokezelo
         static void LengthIndexGenerator() //Generál egy FIX pozíciót egy szám karakternek
         {
             Random r = new Random();
-            lengthIndex = r.Next(1, passLength);            
+            lengthIndex = r.Next(1, passLength);    
         }
 
         static void Save() //Jelszó mentése egy txt fájlba
@@ -71,7 +98,7 @@ namespace jelszokezelo
             string usern = Console.ReadLine();            
 
             StreamWriter sw = new StreamWriter("n.txt", true); //true - hozzáír a fájlhoz
-            sw.WriteLine($"{usern}|{pass}");
+            sw.WriteLine($"{usern} {pass}");
 
             sw.Close();
 
